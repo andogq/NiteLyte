46228190ad3f23b22aaf9e727b6b1fcfb4991c94
Tom Anderson <tom@ando.gq>
2022-02-25 18:53:11 +1100

    Bottom Sheet Working

--- components/Layout/index.js
+++ components/Layout/index.js
@@ -1,16 +1,34 @@
 import { Box } from "@mantine/core";
 
+import { BottomSheet } from "react-spring-bottom-sheet";
+import "react-spring-bottom-sheet/dist/style.css";
+
 export default function Layout({ children }) {
     return (
         <Box>
-            <Box>
+            <Box style={{ background: "cyan" }}>
                 {/* map */}
                 Map stuff wow
             </Box>
-            <Box>
+
+            <BottomSheet
+                open
+                blocking={false}
+                header={(
+                    <Box>
+                        This will be some buttons
+                    </Box>
+                )}
+                snapPoints={state => [
+                    state.headerHeight,
+                    state.maxHeight * 0.75,
+                    state.maxHeight
+                ]}
+                defaultSnap={() => 0}
+            >
                 {/* Bottom sheet, app will go here */}
                 {children}
-            </Box>
+            </BottomSheet>
         </Box>
     );
 }
