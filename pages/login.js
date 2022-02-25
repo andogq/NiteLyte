c66780922c814152c322ef6198ba6d1d5a8fab8b
perrywinkle <isaacyjc@gmail.com>
2022-02-25 21:23:18 +1100

    +auth views. +react-phone-input-2.

--- pages/login.js
+++ pages/login.js
@@ -1,7 +1,28 @@
-import { Box } from "@mantine/core";
+import { Box, TextInput, Button, LoadingOverlay, PasswordInput, Tabs } from "@mantine/core";
+import { useForm } from "@mantine/hooks";
+import { useState } from "react";
+import { User, UserPlus } from "lucide-react";
+import { LoginForm, SignupForm } from "../components/AuthForms";
 
 export default function Login() {
+
+    const [activeTab, setActiveTab] = useState('');
+
+    const _handleTabChange = (tabKey) => {
+        setActiveTab(tabKey)
+    }
+
     return (
-        <Box>Login/Register page</Box>
+        <Box m={10}>
+            <Tabs active={activeTab} onTabChange={_handleTabChange}>
+                <Tabs.Tab label="Login" tabKey="login" icon={<User />}>
+                    <LoginForm/>
+                </Tabs.Tab>
+                <Tabs.Tab label="Sign Up" tabKey="signup" icon={<UserPlus />}>
+                    <SignupForm/>
+                </Tabs.Tab>
+            </Tabs>
+            
+        </Box>
     );
 }
