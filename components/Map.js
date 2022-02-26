11ab4336b5f0088a724279ebcfaacd1de1341038
Tom Anderson <tom@ando.gq>
2022-02-26 11:36:58 +1100

    Add geolocation

--- components/Map.js
+++ components/Map.js
@@ -4,46 +4,33 @@ import mapbox from "mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
 mapbox.accessToken = process.env.NEXT_PUBLIC_MAPBOX_TOKEN;
 
-import { Box } from "@mantine/core";
+import { Box, useMantineTheme } from "@mantine/core";
 import mapboxgl from "mapbox-gl";
+
+const LIGHT_SOURCES = [
+    "council_lights.json",
+    "feature_lights.json"
+];
 
 export default function Map() {
     const map = useRef(null);
     const map_container = useRef(null);
 
-    const [council_lights, set_council_lights] = useState([]);
-    useEffect(() => {
-        if (map.current) map.current.getSource("council_lights").setData({
-            type: "FeatureCollection",
-            features: council_lights.map(light => ({
-                type: "Feature",
-                geometry: {
-                    type: "Point",
-                    coordinates: light[1]
-                },
-                properties: {
-                    lux: light[0]
-                }
-            }))
-        })
-    }, [map.current, council_lights]);
+    const theme = useMantineTheme();
 
-    const [feature_lights, set_feature_lights] = useState([]);
+    const [lights, set_lights] = useState([]);
     useEffect(() => {
-        if (map.current) map.current.getSource("feature_lights").setData({
+        if (map.current) map.current.getSource("lights").setData({
             type: "FeatureCollection",
-            features: feature_lights.map(light => ({
+            features: lights.map(light => ({
                 type: "Feature",
                 geometry: {
                     type: "Point",
                     coordinates: light[1]
-                },
-                properties: {
-                    wattage: light[0]
                 }
             }))
-        })
-    }, [map.current, feature_lights]);
+        });
+    }, [map.current, lights]);
 
     useEffect(() => {
         let _map = new mapboxgl.Map({
@@ -55,7 +42,20 @@ export default function Map() {
         map.current = _map;
 
         _map.on("load", () => {
-            _map.addSource("council_lights", {
+            const label_layer = _map.getStyle().layers.find(layer => layer.type === "symbol")?.id ?? null;
+
+            _map.addControl(
+                new mapboxgl.GeolocateControl({
+                    positionOptions: {
+                        enableHighAccuracy: true
+                    },
+                    trackUserLocation: true,
+                    showUserHeading: true,
+                    showAccuracyCircle: true
+                })
+            );
+            
+            _map.addSource("lights", {
                 type: "geojson",
                 data: {
                     type: "FeatureCollection",
@@ -64,39 +64,34 @@ export default function Map() {
             });
 
             _map.addLayer({
-                id: "council_lights",
-                type: "circle",
-                source: "council_lights",
+                id: "lights",
+                type: "heatmap",
+                source: "lights",
                 paint: {
-                    "circle-color": "yellow"
+                    "heatmap-radius": [
+                        "interpolate",
+                        ["linear"], ["zoom"],
+                        13, 5,
+                        15, 10,
+                        18, 15
+                    ],
+                    "heatmap-color": [
+                        "interpolate",
+                        ["linear"], ["heatmap-density"],
+                        0, "rgba(0, 0, 0, 0)",
+                        0.1, theme.colors.yellow[0],
+                        0.3, theme.colors.yellow[1],
+                        0.5, theme.colors.yellow[2],
+                        0.7, theme.colors.yellow[3],
+                        1, theme.colors.yellow[4]
+                    ],
+                    "heatmap-opacity": 0.9
                 }
-            });
+            }, label_layer);
 
-            fetch("/data/council_lights.json").then(async response => {
+            for (let source of LIGHT_SOURCES) fetch(`/data/${source}`).then(async response => {
                 let data = await response.json();
-                set_council_lights(data);
-            });
-
-            _map.addSource("feature_lights", {
-                type: "geojson",
-                data: {
-                    type: "FeatureCollection",
-                    features: []
-                }
-            });
-
-            _map.addLayer({
-                id: "feature_lights",
-                type: "circle",
-                source: "feature_lights",
-                paint: {
-                    "circle-color": "red"
-                }
-            });
-
-            fetch("/data/feature_lights.json").then(async response => {
-                let data = await response.json();
-                set_feature_lights(data);
+                set_lights(l => [...l, ...data]);
             });
         });
     }, []);
