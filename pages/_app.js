9d4289dd626baa045c8eeb15a8a11ac115060d18
perrywinkle <isaacyjc@gmail.com>
2022-02-26 15:03:37 +1100

    auth working.

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,25 @@
 import "../styles/globals.css";
 import { MantineProvider } from '@mantine/core';
-
+import { useState } from "react";
 import Layout from "../components/Layout";
+import { UserContext } from "../context";
 
 function MyApp({ Component, pageProps }) {
+
+    const [user, setUser] = useState(null);
+
     return (
-        <MantineProvider
-            withGlobalStyles
-            withNormalizeCSS
-            theme={{ colorScheme: 'dark' }}
-        >
-            <Layout>
-                <Component {...pageProps} />
-            </Layout>
-        </MantineProvider>
+        <UserContext.Provider value={{user,setUser}}>
+            <MantineProvider
+                withGlobalStyles
+                withNormalizeCSS
+                theme={{ colorScheme: 'dark' }}
+            >
+                <Layout>
+                    <Component {...pageProps} />
+                </Layout>
+            </MantineProvider>
+        </UserContext.Provider>
     );
 }
 
