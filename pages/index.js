aac67964cf92c9ce64a1f4a4f624b62fefc38b58
perrywinkle <isaacyjc@gmail.com>
2022-02-27 16:15:54 +1100

    made homepage less crappy.

--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,22 @@
-import { Box, Button } from "@mantine/core";
-import Link from "next/link"
+import { Box, Button, Text } from "@mantine/core";
+import { Title } from "@mantine/core";
+
+import Link from "next/link";
 
 export default function Home() {
     return (
-        <>
-            <Box>General Info</Box>
+        <Box>
+            <Title order={2}>We are NiteLyte.</Title>
+
+            <Text my={20}>
+                Nitelyte helps you navigate your way through the night safely
+                by dodging low-light areas, dodgy alleyways, or risky happenings in the city.
+            </Text>
+
             <Link href="/login" passHref>
-                <Button component="a">Next link button</Button>
+                <Button component="a">Take me to the app!</Button>
             </Link>
-        </>
+
+        </Box>
     );
 }
