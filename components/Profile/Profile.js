5e4d9718ebd2cc033f715ba8be7863ecf81ac826
Yiyang Huang <oliviahyy1126@gmail.com>
2022-02-27 11:43:22 +0800

    Fix user name and phone not showing on profile page

--- components/Profile/Profile.js
+++ components/Profile/Profile.js
@@ -8,12 +8,15 @@ export default function ProfilePage({ user }) {
     return (
         <div>
             <Title order={2}>Profile</Title>
-            <br/>
+            <br />
             <SimpleGrid cols={1} spacing="xl">
-                <DisabledOptionButton text={user.name} />
+                <DisabledOptionButton text={user.displayName} />
                 <DisabledOptionButton text={user.email} />
-                <DisabledOptionButton text={user.phone} />
-                <OptionButton text="Emergency Contacts" href="/add_emergency_contact" />
+                <DisabledOptionButton text={user.phoneNumber} />
+                <OptionButton
+                    text="Emergency Contacts"
+                    href="/add_emergency_contact"
+                />
                 <OptionButton text="Settings" href="/settings" />
                 <OptionButton text="Help and Feedback" />
                 <Divider />
